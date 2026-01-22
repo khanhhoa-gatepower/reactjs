35a0141b073bfe5bfe1faba2dfbce99c68f051b7
Khanh Hoa <hoa.lokhanh@powergatesoftware.com>
2026-01-22 11:55:10 +0700

    format project

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import MainHeader from '../components/main-header';
-import NavBar from '../components/nav-bar';
 
 export const MainLayout = () => {
   return (
     <div className="font-inter">
       <MainHeader />
-      <div className="flex">
-        <NavBar />
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 };
